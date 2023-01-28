64832354bd56bd6069b552b04e57aad0d33f65d7
Nikita Sinelnikov <nikita.sinelnikov@lenta.ru>
2023-01-28 23:36:17 +0300

    развернул проект , сделал стили

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,21 @@
+import AccountingCards from '../../widgets/accountingСards/AccountingCards'
+function Main(): React.ReactElement {
+   return (
+      <>
+         <article className=" flex gap-2 min-w-full flex-col flex-1 w-6/6">
+          <AccountingCards></AccountingCards>
+            <section className="flex-1 flex flex-col gap-2 md:gap-3 sm:flex-row-reverse ">
+               <aside className="  bg-white  flex-1  md:min-w-[110px]  sm:max-w-[220px] lg:max-w-[440px] rounded-md shadow-lg">
+                  dsdfsdfs
+               </aside>
+               <div className="flex flex-auto flex-col gap-2 md:gap-3">
+                  <div className="flex-1  bg-white max-h-[300px] rounded-md shadow-lg">32</div>
+                  <div className="flex-1  bg-white rounded-md shadow-lg">32</div>
+               </div>
+            </section>
+         </article>
+      </>
+   );
+}
+
+export default Main;
---
+++ src/widgets/appBar/AppBar.tsx
@@ -0,0 +1,33 @@
+function AppBar(): React.ReactElement {
+   return (
+      <nav className="flex min-w-[50px] lg:min-w-[130px] bg-slate-900 rounded-md shadow-lg  ">
+         <ul>
+            <li>
+               <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-14 h-12"
+               >
+                  <path
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
+                     d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+               </svg>
+            </li>
+            <li>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-12">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
+</svg>
+
+            </li>
+         </ul>
+      </nav>
+   );
+}
+
+export default AppBar;
