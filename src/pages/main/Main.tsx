c0515513fd4653fb11550ba921ee939c9728c9d5
Nikita Sinelnikov <nikita.sinelnikov@lenta.ru>
2023-01-31 19:59:44 +0300

    d

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,5 +1,4 @@
 import AccountingCards from '../../widgets/accountingСards/AccountingCards'
-<<<<<<< HEAD
 function Main():React.ReactElement  {
    return (
       <>
@@ -7,16 +6,6 @@ function Main():React.ReactElement  {
           <AccountingCards></AccountingCards>
             <section className="flex-1 flex flex-col gap-2 md:gap-3 sm:flex-row-reverse ">
                <aside className="  bg-white  flex-1  md:min-w-[110px]  sm:max-w-[220px] lg:max-w-[440px] rounded-md shadow-lg">
-=======
-function Main(): React.ReactElement {
-   return (
-      <>
-         <article className=" flex gap-2 min-w-full flex-col flex-1 w-6/6">
-          <AccountingCards></AccountingCards>
-            <section className="flex-1 flex flex-col gap-2 md:gap-3 sm:flex-row-reverse ">
-               <aside className="  bg-white  flex-1  md:min-w-[110px]  sm:max-w-[220px] lg:max-w-[440px] rounded-md shadow-lg">
-                  dsdfsdfs
->>>>>>> 64832354bd56bd6069b552b04e57aad0d33f65d7
                </aside>
                <div className="flex flex-auto flex-col gap-2 md:gap-3">
                   <div className="flex-1  bg-white max-h-[300px] rounded-md shadow-lg">32</div>
