6b5a1b6302febd372446ee7252183ed489b0b0b1
Nikita Sinelnikov <nikita.sinelnikov@lenta.ru>
2023-02-08 22:03:03 +0300

    CI/CD

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import RegistrationNewUser from "../../features/auth/formRegistrationNewUser/FormRegistrationNewUser";
+import React from 'react';
+import RegistrationNewUser from '../../features/auth/formRegistrationNewUser/FormRegistrationNewUser';
 
 
 function Registration(): React.ReactElement {
-  return (
-    <>
-      <section className="flex-1 flex flex-col items-center justify-center">
-        <RegistrationNewUser />
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className="flex-1 flex flex-col items-center justify-center">
+                <RegistrationNewUser />
+            </section>
+        </>
+    );
 }
 
 export default Registration;
