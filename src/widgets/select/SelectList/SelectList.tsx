06b43fe8d1dc21b45869e8dbacf458c050bdf0b3
Nikita Sinelnikov <nikita.sinelnikov@lenta.ru>
2023-04-09 23:18:59 +0300

    Update Select

--- src/widgets/select/SelectList/SelectList.tsx
+++ src/widgets/select/SelectList/SelectList.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import { categoriesStore } from 'shared/store/categoriesStore/CategoriesStore';
 import { ISelectList } from './interfaces';
+const a = ['e','w','t','ty','yt','tr'];
 
 export const SelectList = observer(({ getValueSelect,style }: ISelectList) => {
     const { categories } = categoriesStore;
@@ -8,16 +9,19 @@ export const SelectList = observer(({ getValueSelect,style }: ISelectList) => {
 
     return (
 
-        <ul className={`flex-1 w-full max-h-20 overflow-y-auto relative bg-slate-900 ${style}`}>
-            {categories?.map(({ categorie }) => {
+        <ul className={`flex-1 absolute left-0  w-full max-h-20 overflow-y-auto  bg-slate-900 ${style}`}>
+            {a?.map((i) => {
                 return (
                     <li
-                        key={categorie}
-                        className="bg-white cursor-pointer  h-8
-                         text-black font-semibold rounded-md shadow-lg px-2 py-1 border-solid border-b-2 hover:bg-slate-500 hover:text-white"
-                        onClick={() => getValueSelect(categorie)}
+                        key={i}
+                        className="bg-white cursor-pointer  h-7
+                         text-black font-semibold my-1 rounded-md shadow-lg px-2 transition-all   hover:bg-slate-500 hover:bor border-amber-600
+
+                         hover:border-solid hover:border-2
+                          hover:text-white"
+                        onClick={() => getValueSelect(i)}
                     >
-                        {categorie}
+                        {i}
                     </li>
                 );
             })}
