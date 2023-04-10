1f3e124385dbf3dcfe15e03bb6399f8491d46389
Nikita Sinelnikov <nikita.sinelnikov@lenta.ru>
2023-04-10 11:00:33 +0300

    Add git ignore

--- src/widgets/select/SelectList/SelectList.tsx
+++ src/widgets/select/SelectList/SelectList.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import { categoriesStore } from 'shared/store/categoriesStore/CategoriesStore';
 import { ISelectList } from './interfaces';
-const a = ['e','w','t','ty','yt','tr'];
+
 
 export const SelectList = observer(({ getValueSelect,style }: ISelectList) => {
     const { categories } = categoriesStore;
@@ -10,18 +10,18 @@ export const SelectList = observer(({ getValueSelect,style }: ISelectList) => {
     return (
 
         <ul className={`flex-1 absolute left-0  w-full max-h-20 overflow-y-auto  bg-slate-900 ${style}`}>
-            {a?.map((i) => {
+            {categories?.map(({ categorie }) => {
                 return (
                     <li
-                        key={i}
+                        key={categorie}
                         className="bg-white cursor-pointer  h-7
                          text-black font-semibold my-1 rounded-md shadow-lg px-2 transition-all   hover:bg-slate-500 hover:bor border-amber-600
 
                          hover:border-solid hover:border-2
                           hover:text-white"
-                        onClick={() => getValueSelect(i)}
+                        onClick={() => getValueSelect(categorie)}
                     >
-                        {i}
+                        {categorie}
                     </li>
                 );
             })}
