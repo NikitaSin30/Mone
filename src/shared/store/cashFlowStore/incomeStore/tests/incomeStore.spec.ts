0959761d49bb027d50132c1fd54f615a4c9fe61d
Nikita Sinelnikov <nikita.sinelnikov@lenta.ru>
2023-04-25 21:37:05 +0300

    merge master

---
+++ src/shared/store/cashFlowStore/incomeStore/tests/incomeStore.spec.ts
@@ -0,0 +1,73 @@
+import { IncomeStore } from '../../IncomeStore';
+import { IIncomeOperation, IIncomeStore } from '../../interfaces';
+import { balanceStore } from '../../BalanceStore';
+
+
+
+describe('class IncomeStore', ()=>{
+
+    const incomeOperation: IIncomeOperation  = {
+        income : 100,
+        sphere : 'IT',
+        date   : new Date(),
+    };
+
+    const income = 100;
+    let incomeStore : IIncomeStore | null;
+    const mockupdateCashAccountSpy = jest.spyOn(balanceStore, 'updateCashAccount');
+
+    beforeEach(() => {
+        incomeStore = new IncomeStore();
+    });
+
+    afterEach(() => {
+        incomeStore = null;
+        jest.clearAllMocks();
+    });
+
+    describe('Method addIncome', () => {
+
+        test('Should add new operation and update income' , () => {
+
+        incomeStore!.addIncome(incomeOperation);
+        expect(incomeStore!.incomeOperations).toContainEqual(incomeOperation);
+        expect(incomeStore!.income).toEqual(100);
+
+        });
+
+        test('Should call method from balanceStore', () => {
+
+
+            incomeStore!.addIncome(incomeOperation);
+            expect(mockupdateCashAccountSpy).toHaveBeenCalledTimes(1);
+
+        });
+
+        test('Should increase length array after add', () => {
+
+            incomeStore!.addIncome(incomeOperation);
+            expect(incomeStore!.incomeOperations).toHaveLength(1);
+
+        });
+
+    });
+
+    describe('Method setIncome', () => {
+        test('Should set income and operations from mongo', () => {
+
+            incomeStore!.setIncomeFromDB(income, [incomeOperation]);
+
+            expect(incomeStore!.income).toEqual(100);
+            expect(incomeStore!.incomeOperations).toContainEqual(incomeOperation);
+        });
+
+        test('Should increase length array after set', () => {
+
+            incomeStore!.setIncomeFromDB(income,[incomeOperation]);
+            expect(incomeStore!.incomeOperations).toHaveLength(1);
+
+        });
+
+    });
+
+});
