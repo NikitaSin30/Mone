7ef6b336cf15da22c372187624a2d110b0ba67f3
Nikita <nikita.sinelnikov@lenta.ru>
2024-01-05 13:42:05 +0700

    button

--- front/src/shared/ui/button/index.tsx
+++ front/src/shared/ui/button/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
-export const Button = ({ textContent, isDisable }: { textContent: string; isDisable: boolean }) => {
+import style from './index.module.less'
+
+export const Button = ({
+  textContent,
+  isDisable,
+  size,
+}: {
+  textContent: string
+  isDisable: boolean
+  size: 'sm' | 'md' | 'lg'
+}) => {
+  const buttonStateStyle: string = isDisable ? style[`button-disabled`] : style[`button-active`]
+
   return (
     <>
-      <button disabled={isDisable}>{textContent}</button>
+      <button
+        className={`${style.button} ${buttonStateStyle} ${style[`button-${size}`]}`}
+        disabled={isDisable}
+      >
+        {textContent}
+      </button>
     </>
   )
 }
